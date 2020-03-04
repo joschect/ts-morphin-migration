2814d1982932bec90b1da4fcae7b680e660794e8
Jon S <joschect@microsoft.com>
2020-03-04 11:36:12 -0800

    renames finished

--- src/__tests__/test/rename_persona.test.ts
+++ src/__tests__/test/rename_persona.test.ts
@@ -1,10 +1,15 @@
-import {Project,SyntaxKind, InterfaceDeclaration } from 'ts-morph';
+import {Project,SyntaxKind, InterfaceDeclaration, Node } from 'ts-morph';
 import {ReplacePersonaImport, ReplaceIPersonaPropsImport} from '../../mods/rename_persona';
 import {utilities} from '../../utilities';
-const project = new Project();
-project.addSourceFilesAtPaths(`${process.cwd()}/**/__tests__/mock/**/persona/**/*.tsx`);
+const personaPath = '/**/__tests__/mock/**/persona/**/*.tsx';
 
 describe('utilities test', () => {
+  let project: Project;
+  beforeEach(()=> {
+   project = new Project();
+   project.addSourceFilesAtPaths(`${process.cwd()}${personaPath}`);
+
+  })
   it('can replace persona with avatar', ()=> {
       const files = project.getSourceFiles();
       files.forEach(ReplacePersonaImport);
@@ -14,7 +19,6 @@ describe('utilities test', () => {
             expect(name.getText()).not.toEqual("Persona");
           });
         });
-
         expect(utilities.findJsxTagInFile(file, "Persona").length).toBe(0);
       });
   });
@@ -38,33 +42,29 @@ describe('utilities test', () => {
                 })).toBe(false);
             }
         })
-
       });
   });
 
   it('can replace PersonaSize with AvatarSize', ()=> {
       const files = project.getSourceFiles();
-      files.forEach(ReplaceIPersonaPropsImport);
-      files.forEach(file => {
-        file.getImportDeclarations().forEach(imp => {
-          imp.getNamedImports().forEach(name => {
-            expect(name.getText()).not.toEqual("PersonaSize");
+      const refe: Node[] = [];
+      files.forEach(foo => {
+        foo.getImportDeclarations().forEach(f => {
+          f.getNamedImports().forEach(name => {
+              if(name.getText() === "PersonaSize") {
+                  let refs = name.getNameNode().findReferencesAsNodes();
+                  refe.concat(refs);
+              }
           });
         });
-        file.forEachDescendant(val=> {
-            switch (val.getKind()) {
-            case SyntaxKind.InterfaceDeclaration:
-                let t_val = val as InterfaceDeclaration
-                const struct = t_val.getStructure();
-                expect((struct.extends as string[])?.some(val => {
-                    return val === "IPersonaProps";
-                })).toBe(false);
-            }
-            return false
-        })
-
       });
-
+      files.forEach(ReplaceIPersonaPropsImport);
+      refe.forEach(val => {
+          // I'm not convinced that this is the best way to test this particular
+          // case, but I'm not sure how else to have a dynamic list of 
+          // all the cases where AvatarSize is actually used.
+          expect(val.getText()).toEqual("AvatarSize");
+      });
   });
 
 });
